c24823d83379e92e19e7f4bd272e5ea55d8ec754
vandit <vasavandit@gmail.com>
2021-04-19 14:55:15 +0530

    fixed exception handeling

--- app.py
+++ app.py
@@ -5,31 +5,33 @@ import covid
 app = Flask(__name__)
 
 
-@app.route('/covid/', methods=['GET', 'POST'])
+@app.route('/covid/', methods=['GET', 'POST'])  # world-wide stats
 def global_results():
     try:
-        return jsonify({"Total Cases": covid.global_info()[0].replace(",", ""),
-                        "Total Deaths": covid.global_info()[1].replace(",", ""),
-                        "Total Recovered": covid.global_info()[2].replace(",", ""),
-                        "Closed Cases": covid.global_info()[3].replace(",", ""),
+        return jsonify({"Total Cases": covid.global_info()[0],
+                        "Total Deaths": covid.global_info()[1],
+                        "Total Recovered": covid.global_info()[2],
+                        "Closed Cases": covid.global_info()[3],
                         "active Cases": covid.global_info()[4]})
     except requests.exceptions.RequestException as e:
         return jsonify({"No Entries Found": str(e)})
 
 
-@app.route('/covid/<country>', methods=['GET', 'POST'])
+@app.route('/covid/<country>', methods=['GET', 'POST'])     # country-wise stats
 def findInfo(country):
     try:
-        return jsonify({"Total Cases": covid.information(country)[0].replace(",", ""),
-                        "Total Deaths": covid.information(country)[1].replace(",", ""),
-                        "Total Recovered": covid.information(country)[2].replace(",", ""),
-                        "Closed Cases": covid.information(country)[3].replace(",", ""),
+        return jsonify({"Total Cases": covid.information(country)[0],
+                        "Total Deaths": covid.information(country)[1],
+                        "Total Recovered": covid.information(country)[2],
+                        "Closed Cases": covid.information(country)[3],
                         "active Cases": covid.information(country)[4]})
     except requests.exceptions.RequestException as e:
         return jsonify({"No Entries Found": str(e)})
+    except IndexError as e:
+        return jsonify({"Error": str(e)})
 
 
-@app.route('/covid/countries', methods=['GET', 'POST'])
+@app.route('/covid/countries', methods=['GET', 'POST'])     # list of all countries
 def all_countries():
     try:
         return jsonify({"All Countries": covid.all_countries()})
